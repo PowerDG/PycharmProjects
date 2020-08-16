3b3ccc1c00b88528bf14fac6966b6402ff5f1859
“powerdg” <1049365046@qq.com>
2020-08-17 00:10:54 +0800

    Collections

---
+++ dgCubeDemo/Collections.py
@@ -0,0 +1,105 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# outhor:xinlan time:
+
+#  ===  列表----[ ]
+nameList = ['猴子', '马云', '王健林', '马化腾']
+
+nameLen = len(nameList)
+
+print(nameLen)
+nameList.append('刘强东')
+
+print('增加1个元素的列表是', nameList)
+del nameList[1]
+
+print('删除第2个元素后的列表是', nameList)
+name1 = nameList[0]
+
+print('查询列表的第一个元素是：', name1)
+print('修改之前的列表是', nameList)
+
+nameList[0] = '孙悟空'
+
+print('修改之后的列表是', nameList)
+
+# ===   定义元组
+
+gafataTuple = ('腾讯', '阿里巴巴', '苹果', '谷歌', 'FB', '亚马逊')
+
+# 元组的长度
+
+gafataLen = len(gafataTuple)
+
+print('元组的长度是：', gafataLen)
+
+# 查询元组元素
+
+print('第1个元素的值', gafataTuple[0])
+
+# List有序、可重复，Set无序，不能重复的
+
+# ===   定义：集合（sets）是不重复的容器
+
+gafataSets = {'腾讯', '阿里巴巴', '苹果', '谷歌', 'FB', '亚马 逊', '亚马逊'}
+
+print(gafataSets)
+
+stockSets = set()
+
+# 使用update()增加元素
+
+stockSets.update(['腾讯', '阿里巴巴', '京东'])
+
+print(stockSets)
+# （2）删除
+
+stockSets.discard('京东')
+
+print(stockSets)
+
+# （3）查找
+
+txBool = '京东' in stockSets
+
+print(txBool)
+
+# （#4）修改
+
+stockSets.discard('京东')
+
+stockSets.update(['京东'])
+
+print(stockSets)
+
+# 字典（映射（键值对））----{ }
+
+# 定义字典
+
+patientDic = {'001': ['猴子', 29, '1型糖尿病', '较差'], '002': ['马云', 34, '2型糖尿病', '好转'], '003': ['王健林', 28, '1型糖尿病', '显著好转'],
+              '004': ['马化腾', 52, '型糖尿病', '好转'], '005': ['王思聪', 30, '1型糖尿病', '好转']}
+
+# ===   字典的操作：
+
+# （1）增加
+
+print(patientDic)
+
+# （2）删除
+
+del patientDic['005']
+
+print(patientDic)
+
+# （3）查询
+
+valueList = patientDic['001']
+
+print(valueList)
+# （4）修改
+
+print('修改之前的，病人信息：', patientDic)
+
+patientDic['001'] = ['猴子', 29, '1型糖尿病', '好转']
+
+print('修改之后的，病人信息：', patientDic)
