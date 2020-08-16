ea2d41d69c8eca045b7b1a5798486b2316385961
“powerdg” <1049365046@qq.com>
2020-08-17 00:29:44 +0800

    Create ConditionalStatements.py

---
+++ dgCubeDemo/ConditionalStatements.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# outhor:xinlan time:
+
+
+# switch = {"valueA":functionA,"valueB":functionB,"valueC":functionC}
+# try:
+#　　switch["value"]() #执行相应的方法。
+# except KeyError as e:
+#       pass 或 functionX #执行default部分
+
+switch = {
+    "a":lambda x:x*2,
+    "b":lambda x:x*3,
+    "c":lambda x:x**x
+}
+try:
+    swtich["c"](6)
+except KeyError as e:
+    pass
